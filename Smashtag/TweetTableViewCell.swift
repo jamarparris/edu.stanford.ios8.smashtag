5c8a878f81d3c9b992b54487fa1119899cb578d8
Jamar Parris <jamar@jamarparris.com>
2015-07-22 23:14:06 -0400

    Added NSMutableAttributedString logic to highlight hashtags, URLs and user mentions.

--- Smashtag/TweetTableViewCell.swift
+++ Smashtag/TweetTableViewCell.swift
@@ -30,12 +30,38 @@ class TweetTableViewCell: UITableViewCell
         
         //load new information from our tweet (if any)
         if let tweet = self.tweet {
+            
+            /*
             tweetTextLabel?.text = tweet.text
             if tweetTextLabel?.text != nil {
                 for _ in tweet.media {
                     tweetTextLabel.text! += " 📷"
                 }
+            } */
+            
+            //create attributed string from tweet text
+            var tweetTextAttributedString = NSMutableAttributedString(string: tweet.text)
+            
+            
+            //set color attributes on hashtags
+            for hashtag in tweet.hashtags {
+                let hashtagColor = UIColor.grayColor()
+                tweetTextAttributedString.setAttributes([NSForegroundColorAttributeName: hashtagColor], range: hashtag.nsrange)
             }
+            
+            //set color attributes on URLs
+            for url in tweet.urls {
+                let urlColor = UIColor.blueColor()
+                tweetTextAttributedString.setAttributes([NSForegroundColorAttributeName: urlColor], range: url.nsrange)
+            }
+            
+            //set color attributes on mentioned users
+            for userMention in tweet.userMentions {
+                let urlColor = UIColor.redColor()
+                tweetTextAttributedString.setAttributes([NSForegroundColorAttributeName: urlColor], range: userMention.nsrange)
+            }
+            
+            tweetTextLabel?.attributedText = tweetTextAttributedString
             
             tweetScreenNameLabel?.text = "\(tweet.user)" //tweet.user.description
             
